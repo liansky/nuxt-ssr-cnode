9eae34771b9491d233d8394a1cfc0aedf3ed9564
lianheguo <461541878@qq.com>
2019-01-08 23:07:12 +0800

    eslint sass 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,11 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
-    'plugin:vue/recommended'
+    'plugin:vue/recommended',
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    // standard 覆盖上面配置 https://standardjs.com/rules-zhcn.html#javascript-standard-style
+    'standard'
   ],
   // required to lint *.vue files
   plugins: [
@@ -29,6 +33,29 @@ module.exports = {
   },
   // add your custom rules here
   rules: {
+    // https://vuejs.github.io/eslint-plugin-vue/rules/order-in-components.html
+    "vue/order-in-components": ["error", {
+      "order": [
+        "el",
+        "name",
+        "data",
+        "parent",
+        "functional",
+        ["delimiters", "comments"],
+        ["components", "directives", "filters"],
+        "extends",
+        "mixins",
+        "inheritAttrs",
+        "model",
+        ["props", "propsData"],
+        "computed",
+        "watch",
+        "LIFECYCLE_HOOKS",
+        "methods",
+        ["template", "render"],
+        "renderError"
+      ]
+    }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
